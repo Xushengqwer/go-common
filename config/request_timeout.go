3a410b552a2fe4b14e1dd9a8dc971142c6a3efd2
Xushengqwer <127616011+Xushengqwer@users.noreply.github.com>
2025-04-28 02:20:42 +0800

    全局请求超时配置

--- config/request_timeout.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "time"
-
-type RequestTimeout struct {
-	RequestTimeout time.Duration `mapstructure:"request_timeout" yaml:"request_timeout"`
-}
--- config/server.go
+++ config/server.go
@@ -1,5 +1,9 @@
 package config
 
-type Server struct {
-	ListenAddr string `yaml:"listenAddr" json:"listenAddr" yaml:"listenAddr"`
+import "time"
+
+type ServerConfig struct {
+	ListenAddr     string        `mapstructure:"listen_addr" yaml:"listen_addr"` // 添加监听地址
+	Port           string        `mapstructure:"port" yaml:"port"`
+	RequestTimeout time.Duration `mapstructure:"requestTimeout" yaml:"requestTimeout"`
 }
