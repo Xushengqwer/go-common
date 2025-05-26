2123adf387b41a8e1081234bd672cb50096a86c4
Xushengqwer <127616011+Xushengqwer@users.noreply.github.com>
2025-05-26 17:06:59 +0800

    新增一个公共错误，删除一下重复文件

--- commonerrors/commonerrors.go
+++ commonerrors/commonerrors.go
@@ -7,3 +7,5 @@ var ErrRepoNotFound = errors.New("数据没找到")
 var ErrServiceBusy = errors.New("服务繁忙，请稍后再试")
 
 var ErrSystemError = errors.New("系统错误，请稍后重试")
+
+var ErrUserNotLoggedIn = errors.New("用户未登录") // 新增：用户未登录错误
