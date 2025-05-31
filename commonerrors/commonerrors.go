833415a00db1b46425af080b4135272fe48944f6
Xushengqwer <127616011+Xushengqwer@users.noreply.github.com>
2025-05-31 14:03:52 +0800

    增加一个公共错误

--- commonerrors/commonerrors.go
+++ commonerrors/commonerrors.go
@@ -9,3 +9,5 @@ var ErrServiceBusy = errors.New("服务繁忙，请稍后再试")
 var ErrSystemError = errors.New("系统错误，请稍后重试")
 
 var ErrUserNotLoggedIn = errors.New("用户未登录") // 新增：用户未登录错误
+
+var ErrThirdPartyServiceError = errors.New("第三方服务出现故障")
